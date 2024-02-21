678df5473ee035d82fa7565eba3d2d4ccee7d26a
Oltion-Koceku <koceku17@gmail.com>
2024-02-21 15:47:51 +0100

    [add]script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-// Prompt da chiedere all'utente
-
-const kM = prompt("Quanti chilometri vuoi percorrere?");
-
-const eta = prompt("Quanti anni hai?")
-console.log(kM, eta);
-
+// elementi
+const km = document.querySelector(".inputkm");
+const eta = document.querySelector(".selezioneEta")
 let prezzoConSconti;
+
+// button
+const generator = document.querySelector(".generator")
+
+// click
+generator.addEventListener("click", function(event){
+  event.preventDefault();
+  console.log(km.value);
+
+})
+
+generator.addEventListener("click", sceltaEta)
+
 // Calcoli
 
-const prezzoBiglietto = kM * 0.21;
+const prezzoBiglietto = km.value * 0.21;
 
 const prezzoMinorenne = (prezzoBiglietto * 20 )/100;
 const prezzoOver = (prezzoBiglietto * 40 )/100;
+
+console.log(prezzoMinorenne, prezzoBiglietto, prezzoOver);
 
 if(eta < 18){
   prezzoConSconti = prezzoBiglietto - prezzoMinorenne;
@@ -21,10 +32,47 @@ if(eta < 18){
   prezzoConSconti = prezzoBiglietto
 }
 
+
+console.log(prezzoConSconti);
 // Prezzo da pagare
 
 let prezzoTotale = prezzoConSconti.toFixed(2);
 
-document.getElementById("output").innerHTML += prezzoTotale
-console.log(prezzoBiglietto, prezzoMinorenne, prezzoOver);
+
 console.log(prezzoConSconti, prezzoTotale);
+// function
+
+function sceltaEta(){
+  console.log(eta.value);
+
+}
+
+
+
+
+
+
+
+// let prezzoConSconti;
+// // Calcoli
+
+// const prezzoBiglietto = kM * 0.21;
+
+// const prezzoMinorenne = (prezzoBiglietto * 20 )/100;
+// const prezzoOver = (prezzoBiglietto * 40 )/100;
+
+// if(eta < 18){
+//   prezzoConSconti = prezzoBiglietto - prezzoMinorenne;
+// }else if(eta > 65){
+//   prezzoConSconti = prezzoBiglietto - prezzoOver;
+// }else{
+//   prezzoConSconti = prezzoBiglietto
+// }
+
+// // Prezzo da pagare
+
+// let prezzoTotale = prezzoConSconti.toFixed(2);
+
+// document.getElementById("output").innerHTML += prezzoTotale
+// console.log(prezzoBiglietto, prezzoMinorenne, prezzoOver);
+// console.log(prezzoConSconti, prezzoTotale);
